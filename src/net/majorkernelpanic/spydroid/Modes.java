8fb8c5c9b6d57c133b12eeaf37f93f6c433a43a9
Marcelo Martins <martins@cs.brown.edu>
2012-11-28 00:56:41 -0500

    Declared modes

---
+++ src/net/majorkernelpanic/spydroid/Modes.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package net.majorkernelpanic.spydroid;
+
+
+/**
+ * @author Marcelo Martins <martins@cs.brown.edu>
+ *
+ * Application-mode labels
+ */
+public class Modes {
+
+	public static final String HD_STREAMING = "HD Streaming";
+	public static final String SD_STREAMING = "SD Streaming";
+	public static final String AUDIO_ONLY = "Audio-Only Streaming";
+}
